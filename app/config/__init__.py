49e90b496d306b9cdbe15249e39de1a23b90e964
Jose David Gómez <jose.gomez14@udea.edu.co>
2025-01-21 23:59:16 -0500

    feat: :sparkles: Add Azure storage configuration and implement file upload/delete services
    Co-authored-by: daniel-lujan <daniel.lujan@udea.edu.co>

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Tiendify"
     DATABASE_URL: str
+    AZURE_STORAGE: str
+    AZURE_PUBLIC_CONTAINER: str
 
     class Config:
         env_file = ".env"
